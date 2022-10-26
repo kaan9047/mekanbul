4cccbc5b2f4ad9189cbf0541d586f6069d4b2f04
kaan9047 <66495309+kaan9047@users.noreply.github.com>
2022-10-26 10:53:03 +0300

    odev3

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,77 @@
+var express = require("express");
+var router = express.Router();
+
+const anaSayfa = function (req, res) {
+  res.render("anasayfa", {
+    "baslik": "Ana Sayfa",
+    "sayfaBaslik": {
+      "siteAd": "MekanBul",
+      "slogan": "Civardaki Mekanları Keşfet!",
+    },
+    "mekanlar": [
+      {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden",
+        "puan": "4",
+        "imkanlar": ["Dünya Kahveleri", "Yiyecek", "Hızlı Wifi Bağlantısı"],
+        "mesafe": "100m",
+      },
+      {
+        "ad": "Gloria Jeans",
+        "adres": "Sdü Doğu Kampüsü",
+        "puan": "3",
+        "imkanlar": ["Kahve", "Çay", "Pasta"],
+        "mesafe": "5km",
+      },
+    ],
+  });
+};
+
+const mekanBilgisi = function (req, res) {
+  res.render("mekanbilgisi", 
+  { 
+    "baslik": "Mekan Bilgisi",
+    "mekanBaslik": "Starbucks",
+    "mekanDetay": {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden AVM",
+        "puan": "4",
+        "imkanlar": ["Dünya Kahveleri", "Kekler", "Pastalar"],
+        "koordinatlar": {
+            "enlem": "37.7",
+            "boylam": "30.5"
+        },
+        "saatler": [
+            {
+                "gunler": "Pazartesi-Cuma",
+                "acilis": "9:00-23:00",
+                "kapali": "false"
+            },
+            {
+                "gunler": "Cumartesi-Pazar",
+                "acilis": "10:00-22:00",
+                "kapali": "false"
+            }
+        ],
+        "yorumlar": [
+            {
+                "yorumYapan": "Berat Sevim",
+                "puan": "4",
+                "tarih": "20 Ekim 2022",
+                "yorumMetni": "Kahveler iyi."
+            }
+        ]
+    }
+  
+ });
+};
+
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Ekle" });
+};
+
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,15 @@
+
+
+
 var express = require('express');
 var router = express.Router();
-var ctrlMain=require('../controllers/main');
-
+var ctrMekanlar=require('../controllers/mekanlar');
+var ctrDigerleri=require('../controllers/digerleri');
 
 /* GET home page. */
-
-
-router.get('/',ctrlMain.index);
-
+router.get('/',ctrMekanlar.anaSayfa);
+router.get('/mekan',ctrMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni',ctrMekanlar.yorumEkle);
+router.get('/hakkinda',ctrDigerleri.hakkinda);
 
 module.exports = router;
